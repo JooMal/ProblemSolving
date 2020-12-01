53ab53248dad063c72c91ec85acc05a100c7bc0d
JooMal <yl95yl@naver.com>
2020-12-02 00:31:11 +0900

    update

---
+++ 2217.cpp
@@ -0,0 +1,43 @@
+﻿#include "pch.h"
+
+/*
+문제 제목 : 거스름돈
+카테고리 : 그리디 (1/30)
+풀이 :
+	10 => 10
+	10,15 => 20
+	15 => 15
+	15,20 => 30
+	20 => 20
+	10,15,20 => 30
+	5,15,20 => 30 (5를 제외)
+*/
+#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+int N;
+
+int main()
+{
+	int ans = -1;
+	cin >> N;
+	vector<int> rope_w;
+	while (N--) {
+		int w;
+		cin >> w;
+		rope_w.push_back(w);
+	}
+	sort(rope_w.begin(), rope_w.end(), greater<>());
+	int cnt = 1;
+	vector<int>::iterator it;
+	for (it = rope_w.begin(); it < rope_w.end(); it++) {
+		ans = max(ans, (*it)*cnt);
+		cnt++;
+	}
+	cout << ans;
+	return 0;
+}
---
+++ 5585.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+#include <deque>
+#include <algorithm>
+
+using namespace std;
+
+int money;
+int ans = 0;
+
+int makeChange(int money, int En) {
+	int tmp_money = money;
+	if (tmp_money - En >= 0) {
+		tmp_money -= En;
+		ans++;
+		return tmp_money;
+	}
+	return money;
+}
+int ens[6] = { 500,100,50,10,5,1 };
+int main()
+{
+	cin >> money;
+	money = 1000 - money;
+
+	while (money) {
+		for (int i = 0; i < 6; i++) {
+			int tmp = makeChange(money, ens[i]);
+			if (tmp < money) {
+				money = tmp; break;
+			}
+		}
+	}
+
+	cout << ans;
+	return 0;
+}
